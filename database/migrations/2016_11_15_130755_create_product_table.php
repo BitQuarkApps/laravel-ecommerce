ab1049184d38eba7d20041929242cbd2380036dd
Ing. Fernando <fernando@bitquark.com.mx>
2021-02-16 18:47:30 -0600

    ...

--- database/migrations/2016_11_15_130755_create_product_table.php
+++ database/migrations/2016_11_15_130755_create_product_table.php
@@ -28,8 +28,8 @@ class CreateProductTable extends Migration
             $table->double('price', 10, 2);
             $table->string('brand', 30)->nullable();
             $table->enum('condition', array_keys(trans('globals.product_conditions')));
-            $table->json('tags')->nullable();
-            $table->json('features');
+            $table->text('tags')->nullable();
+            $table->text('features');
             $table->double('rate_val', 10, 2)->nullable();
             $table->integer('rate_count')->nullable();
             $table->integer('sale_counts')->unsigned();
